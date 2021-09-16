a1f43e038e9c7cfabf19e8f9f9478694e4c06395
Florian Cassayre <florian.cassayre@gmail.com>
2021-09-16 09:55:33 +0200

    All derived errors classes must have their prototype patched

--- src/parse/error.ts
+++ src/parse/error.ts
@@ -1,3 +1,12 @@
+const patchPrototype = <T extends Error>(instance: T, target: any) => {
+    const proto = target.prototype;
+    if (Object.setPrototypeOf) {
+        Object.setPrototypeOf(instance, proto);
+    } else {
+        (this as any).__proto__ = proto; // eslint-disable-line no-proto
+    }
+};
+
 /**
  * The base type for all Gedcom related errors. All errors are currently also instances of {@link ErrorParse}.
  */
@@ -6,18 +15,14 @@ export abstract class ErrorGedcomBase extends Error {
         super(message);
 
         // See https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
-        const proto = new.target.prototype;
-        if (Object.setPrototypeOf) {
-            Object.setPrototypeOf(this, proto);
-        } else {
-            (this as any).__proto__ = proto; // eslint-disable-line no-proto
-        }
+        patchPrototype(this, new.target);
     }
 }
 
 export class ErrorParse extends ErrorGedcomBase {
     constructor(public readonly message: string) {
         super(message);
+        patchPrototype(this, new.target);
     }
 }
 
@@ -26,44 +31,71 @@ export class ErrorInvalidFileType extends ErrorParse {}
 export class GedcomErrorDecoding extends ErrorParse {
     constructor(message: string, public readonly illegalCode: number) {
         super(message);
+        patchPrototype(this, new.target);
     }
 }
 
 export class ErrorUnsupportedCharset extends ErrorParse {
     constructor(message: string, public readonly charset: string) {
         super(message);
+        patchPrototype(this, new.target);
     }
 }
 
 export class ErrorTokenization extends ErrorParse {
     constructor(message: string, public readonly lineNumber: number, public readonly line: string) {
         super(message);
+        patchPrototype(this, new.target);
     }
 }
 
 export class ErrorTreeSyntax extends ErrorParse {
     constructor(message: string, public readonly lineNumber: number) {
         super(message);
+        patchPrototype(this, new.target);
     }
 }
 export class ErrorInvalidNesting extends ErrorTreeSyntax {
     constructor(message: string, public readonly lineNumber: number, public readonly currentLevel: number, public readonly level: number) {
         super(message, lineNumber);
+        patchPrototype(this, new.target);
     }
 }
 export class ErrorInvalidConcatenation extends ErrorTreeSyntax {
     constructor(message: string, public readonly lineNumber: number, public readonly kind: string) {
         super(message, lineNumber);
+        patchPrototype(this, new.target);
     }
 }
-export class ErrorInvalidRecordDefinition extends ErrorTreeSyntax {}
+export class ErrorInvalidRecordDefinition extends ErrorTreeSyntax {
+    constructor(message: string, lineNumber: number) {
+        super(message, lineNumber);
+        patchPrototype(this, new.target);
+    }
+}
 
-export class ErrorTreeStructure extends ErrorParse {}
-export class ErrorEmptyTree extends ErrorTreeStructure {}
+export class ErrorTreeStructure extends ErrorParse {
+    constructor(message: string) {
+        super(message);
+        patchPrototype(this, new.target);
+    }
+}
+export class ErrorEmptyTree extends ErrorTreeStructure {
+    constructor(message: string) {
+        super(message);
+        patchPrototype(this, new.target);
+    }
+}
 
-export class ErrorIndexing extends ErrorParse {}
+export class ErrorIndexing extends ErrorParse {
+    constructor(message: string) {
+        super(message);
+        patchPrototype(this, new.target);
+    }
+}
 export class ErrorDuplicatePointer extends ErrorIndexing {
     constructor(message: string, public readonly lineNumber: number, public readonly lineNumberOriginalDefinition: number, public readonly pointer: string) {
         super(message);
+        patchPrototype(this, new.target);
     }
 }
